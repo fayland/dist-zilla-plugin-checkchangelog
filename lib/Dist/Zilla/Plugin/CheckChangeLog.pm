c22e7761c870397b26d2b2adc5ca14617c00a749
starbelly <starbelly@pobox.com>
2016-07-04 00:38:34 -0500

    Make CPANTS and POD::Coverage happy : )
     - Use MetaJSON plugin (Happy CPANTS)
     - Specify minimum perl version (5.004) (Happy CPANTS)
     - Add bare POD setup_installer and check_file_version
       methods POD::Coverage was complaining about
     - Tidy up CheckChangeLog.pm
     - Add .gitignore

--- lib/Dist/Zilla/Plugin/CheckChangeLog.pm
+++ lib/Dist/Zilla/Plugin/CheckChangeLog.pm
@@ -2,9 +2,87 @@ package Dist::Zilla::Plugin::CheckChangeLog;
 
 # ABSTRACT: Dist::Zilla with Changes check
 
+use 5.004;
 use Moose;
 use Moose::Autobox;
 with 'Dist::Zilla::Role::InstallTool';
+
+has filename => (
+    is      => 'ro',
+    isa     => 'Str',
+    default => '',
+);
+
+sub setup_installer {
+    my ( $self, undef ) = @_;
+
+    my @change_files;
+    my $filename = $self->{filename};
+    if ($filename) {
+        my $file = $self->zilla->root->file($filename);
+        die "[CheckChangeLog] $! $file\n" unless -e $file;
+        push @change_files,
+          Dist::Zilla::File::OnDisk->new( { name => $filename } );
+    }
+    else {
+        # grep files to check if there is any Changes file
+        my $filter = sub {
+            $_->name =~ m/Change/i
+              and $_->name ne $self->zilla->main_module->name;
+        };
+
+        my $files = $self->zilla->files->grep($filter);
+        undef $filter;
+        if ( scalar @$files ) {
+            push @change_files, @$files;
+        }
+        else {
+            die "[CheckChangeLog] No changelog file found.\n";
+        }
+    }
+
+    my $zilla_ver = $self->zilla->version;
+    foreach my $file (@change_files) {
+        my $ok = $self->check_file_for_version( $file->content, $zilla_ver );
+        if ($ok) {
+            $self->log( "[CheckChangeLog] OK with " . $file->name );
+            return;
+        }
+
+        # XXX? prompt to edit?
+    }
+
+    print "[CheckChangeLog] No Change Log in "
+      . join( ', ', map { $_->name } @change_files ) . "\n";
+    die "[CheckChangeLog] Please edit\n";
+
+    return;
+}
+
+sub check_file_for_version {
+    my ( $self, $content, $version ) = @_;
+
+    my @lines = split( /\n/, $content );
+    foreach (@lines) {
+
+        # no blanket lines
+        next unless /\S/;
+
+        # skip things that look like bullets
+        next if /^\s+/;
+        next if /^\*/;
+        next if /^\-/;
+
+        # seen it?
+        return 1 if /\Q$version\E/;
+    }
+    return 0;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
 
 =head1 SYNOPSIS
  
@@ -17,70 +95,16 @@ with 'Dist::Zilla::Role::InstallTool';
 
 =head1 DESCRIPTION
 
-The code is mostly a copy-paste of L<ShipIt::Step::CheckChangeLog>
- 
+The code is mostly a copy-paste of L<ShipIt::Step::CheckChangeLog> 
+
+=head1 METHODS
+
+=head2 setup_installer
+
+=head2 check_file_for_version($content_str, $version_str)
+
+=head1 AUTHORS
+
+Fayland Lam, C<< <fayland at gmail.com> >>
+
 =cut
-
-has filename => (
-  is  => 'ro',
-  isa => 'Str',
-  default => '',
-);
-
-sub setup_installer {
-    my ($self, $arg) = @_;
-
-    my @change_files;
-    my $filename = $self->{filename};
-    if ( $filename ) {
-        my $file = $self->zilla->root->file( $filename );
-        die "[CheckChangeLog] $! $file\n" unless -e $file;
-        push @change_files, Dist::Zilla::File::OnDisk->new({ name => $filename });
-    } else {
-        # grep files to check if there is any Changes file
-        my $files = $self->zilla->files
-            ->grep( sub {
-                $_->name =~ m/Change/i and $_->name ne $self->zilla->main_module->name
-            } );
-        if (scalar @$files) {
-            push @change_files, @$files;
-        } else {
-            die "[CheckChangeLog] No changelog file found.\n";
-        }
-    }
-    
-    foreach my $file ( @change_files ) {
-        if ( $self->check_file_for_version( $file->content, $self->zilla->version ) ) {
-            $self->log("[CheckChangeLog] OK with " . $file->name);
-            return;
-        }
-        # XXX? prompt to edit?
-    }
-    
-    print "[CheckChangeLog] No Change Log in " . join(', ', map { $_->name } @change_files) . "\n";
-    die "[CheckChangeLog] Please edit\n";
-
-    return;
-}
-
-sub check_file_for_version {
-    my ( $self, $content, $version ) = @_;
-    
-    my @lines = split(/\n/, $content);
-    foreach (@lines) {
-        # no blanket lines
-        next unless /\S/;
-        # skip things that look like bullets
-        next if /^\s+/;
-        next if /^\*/;
-        next if /^\-/;
-        # seen it?
-        return 1 if /\Q$version\E/;
-    }
-    return 0;
-}
-
-__PACKAGE__->meta->make_immutable;
-no Moose;
-
-1;
